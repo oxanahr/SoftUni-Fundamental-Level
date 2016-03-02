7a7a3a9c1f828837fc939679c05b354c6fd92875
oxanahr <oxana.hristova@gmail.com>
2016-03-02 22:49:16 +0200

    first
    first

---
+++ Java-Fundamentals/Homeworks/Introduction to Java/CurrentDateAndTime.java
@@ -0,0 +1,18 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CurrentDateAndTime {
+    public  static void main(String[] args){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+        // get current date time with Date()
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+
+        //get current date time with Calendar()
+        Calendar cal = Calendar.getInstance();
+        System.out.println(dateFormat.format(cal.getTime()));
+    }
+}
